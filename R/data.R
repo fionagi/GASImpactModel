94de0ce1604b412afcc2740cac9fb0d34b416cc6
fionagi <fiona.giannini@telethonkids.org.au>
2021-07-22 14:45:53 +0800

    Add pop data

---
+++ R/data.R
@@ -0,0 +1,15 @@
+# data.R - This file includes documentation of the data used.
+
+#' 2020 Population by age group
+#'
+#' Total population (both sexes combined) by 5-year age group (in thousands).
+#' Data set extracted from "Annual Population by Age Groups - Both Sexes" obtained
+#' from source.
+#'
+#' @format A data table containing 251 observations of 22 variables.
+#' \describe{
+#'   \item{Country}{Region, subregion, country or area}
+#'   \item{0-4, 5-9, ..., 95-99, 100+}{5-year age groups}
+#' }
+#' @source \url{https://population.un.org/wpp/Download/Standard/Population/}
+"data.popbyage2020"
