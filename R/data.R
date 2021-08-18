b9c8d5f6e5b9c4c8577c0b7e92de12ef8769e61b
fionagi <fiona.giannini@telethonkids.org.au>
2021-08-18 14:07:16 +0800

    Update data

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 # data.R - This file includes documentation of the data used.
 
-#' Countries by UN regions
+#' Countries by United Nations regions
 #'
 #'
 #' @format A data table containing 230 observations of 2 variables.
@@ -11,19 +11,36 @@
 #' @source \url{https://population.un.org/wpp/Download/Files/4_Metadata/WPP2019_F01_LOCATIONS.}
 "data.region"
 
-#' Population by age group
+#' Population by age - 2020
 #'
-#' Total population (both sexes combined) for 2020 by 5-year age group (in thousands).
-#' Data set extracted from "Annual Population by Age Groups - Both Sexes" obtained
-#' from source.
+#' Total population (both sexes combined) for 2020 by 1-year age
+#' groups (in thousands).
+#' Data set extracted from United Nations data "Annual Population by Age - Both Sexes"
+#' obtained from source.
 #'
-#' @format A data table containing 251 observations of 22 variables.
+#' @format A data table containing 251 observations of 102 variables.
 #' \describe{
 #'   \item{Country}{Region, subregion, country or area}
-#'   \item{0-4, 5-9, ..., 95-99, 100+}{5-year age groups}
+#'   \item{0, 1, ..., 100}{population in 1-year age groups}
 #' }
-#' @source \url{https://population.un.org/wpp/Download/Standard/Population/}
+#' @source \url{https://population.un.org/wpp/Download/Standard/Interpolated/}
 "data.popbyage2020"
+
+#' Population by age - 2020 - 2100
+#'
+#' Total interpolated population (both sexes combined) for 2020 - 2100
+#' by 1-year age groups (in thousands).
+#' Data set extracted from United Nations data "Annual Population by Age - Both Sexes"
+#' obtained from source.
+#'
+#' @format A data table containing 20331 observations of 103 variables.
+#' \describe{
+#'   \item{Country}{Region, subregion, country or area}
+#'   \item{Year}{2020 - 2100}
+#'   \item{0, 1, ..., 100}{population in 1-year age groups}
+#' }
+#' @source \url{https://population.un.org/wpp/Download/Standard/Interpolated/}
+"data.popbyage.pred"
 
 #' Incidence, death rates and DALYs for rheumatic heart disease (RHD)
 #'
@@ -63,9 +80,9 @@
 #' @source \url{http://ghdx.healthdata.org/gbd-results-tool}
 "data.cellulitis2019"
 
-#' Probability of mortality
+#' Probability of mortality - 2019
 #'
-#' Probability of dying between ages x and x+1 (2019). Data from the Global
+#' Probability of dying between ages x and x+n (2019). Data from the Global
 #' Health Observatory, WHO
 #'
 #' @format A data table containing 6954 observations of 5 variables.
@@ -80,5 +97,38 @@
 #' @source \url{https://www.who.int/data/gho/data/indicators/indicator-details/GHO/gho-ghe-life-tables-by-country}
 "data.mortality2019"
 
+#' Probability of mortality - 2020 - 2050
+#'
+#' Probability of dying between ages x and x+n by country and period.
+#' Data set extracted from United Nations data "Abridged Life Table - Both Sexes"
+#' obtained from source.
+#'
+#' @format A data table containing 31626 observations of 5 variables.
+#' \describe{
+#'   \item{Location}{Region, subregion, country or area}
+#'   \item{Period}{country or area}
+#'   \item{Sex}{female, male}
+#'   \item{Age group}{age groups, <1, 1 to 4, then 5-year age groups until 85 plus}
+#'   \item{Value}{probability of mortality}
+#' }
+#' @source \url{https://population.un.org/wpp/Download/Standard/Mortality/}
+"data.mortality.pred2050"
+
+#' Probability of mortality - 2050 - 2100
+#'
+#' Probability of dying between ages x and x+n by country and period.
+#' Data set extracted from United Nations data "Abridged Life Table - Both Sexes"
+#' obtained from source.
+#'
+#' @format A data table containing 31626 observations of 5 variables.
+#' \describe{
+#'   \item{Location}{Region, subregion, country or area}
+#'   \item{Period}{country or area}
+#'   \item{Sex}{female, male}
+#'   \item{Age group}{age groups, <1, 1 to 4, then 5-year age groups until 85 plus}
+#'   \item{Value}{probability of mortality}
+#' }
+#' @source \url{https://population.un.org/wpp/Download/Standard/Mortality/}
+"data.mortality.pred2100"
 
 
